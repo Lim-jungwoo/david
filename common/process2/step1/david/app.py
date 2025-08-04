911d3ecf2cb1d8e7e84a06ecf1bcfedad76889c9
Lim-jungwoo <72790617+Lim-jungwoo@users.noreply.github.com>
2025-08-04 15:34:02 +0900

    Feat: 공통과정 과정2 단계1 문제2
    웹에서 app.py에 test2 함수 추가

--- common/process2/step1/david/app.py
+++ common/process2/step1/david/app.py
@@ -57,6 +57,10 @@ def home():
 def menu():
     return render_template("menu.html")
 
+@app.route("/test2")
+def test2():
+    return render_template("test2.html")
+
 if __name__ == '__main__':
     print('디버그 모드: ' + ('True' if __debug__ == True else 'False'))
     app.run('0.0.0.0', 80, debug=True)
