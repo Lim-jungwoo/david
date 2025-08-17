744a7d5c3f383fc08ede4087357290a18f84b8a1
LimJungWoo <presidentljw@naver.com>
2025-08-18 08:51:38 +0900

    test1 함수 코드를 삭제 - 기획 변경으로 해당 테스트 코드 삭제

--- common/process2/step2/david/app.py
+++ common/process2/step2/david/app.py
@@ -57,14 +57,6 @@ def home():
 def menu():
     return render_template("menu.html")
 
-@app.route("/test1")
-def test1():
-    return render_template("test1.html")
-
-@app.route("/test1")
-def test1():
-    return render_template("test1.html")
-
 if __name__ == '__main__':
     print('디버그 모드: ' + ('True' if __debug__ == True else 'False'))
     app.run('0.0.0.0', 80, debug=True)
