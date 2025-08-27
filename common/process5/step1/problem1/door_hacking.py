5fd69726c547b3d1a8dd291c3ce257d10c0b90a8
LimJungWoo <presidentljw@naver.com>
2025-08-27 14:39:59 +0900

    feat: 공통과정 과정5 단계1 문제1 Lock을 사용한 멀티 프로세스

--- common/process5/step1/problem1/door_hacking.py
+++ common/process5/step1/problem1/door_hacking.py
@@ -1,12 +1,17 @@
+from __future__ import annotations
+
 # from zipfile import ZipFile, BadZipFile
 import pyzipper
 import zlib
 import time
 from datetime import datetime
 import multiprocessing
+from multiprocessing.synchronize import Event as EventClass
+from multiprocessing.sharedctypes import Synchronized
+from multiprocessing import Process, Value
 
-# ZIP_PATH = 'emergency_storage_key.zip'
-ZIP_PATH = 'test.zip'
+ZIP_PATH = 'emergency_storage_key.zip'
+# ZIP_PATH = 'test.zip'
 EXTRACT_PATH = 'extract'
 PASSWORD_PATH = 'password.txt'
 PASSWORD_LENGTH = 6
@@ -31,34 +36,36 @@ def make_password(count: int, length: int) -> str:
         password.append(CHARSET[digit])
     return ''.join(password)
 
-    # for i in range(length, 0, -1):
-    #     if i == 1:
-    #         password += CHARSET[count % len(CHARSET)]
-    #     else:
-    #         index = (int)(count / (len(CHARSET) ** (i - 1)))
-    #         if index >= len(CHARSET):
-    #             index %= len(CHARSET)
-    #         password += CHARSET[index]
 
-
-def unlock_zip():
-    count = 0
+def unlock_zip(count: Synchronized[int], stop_event: EventClass):
     start_time = time.time()
     last_report = start_time
     started_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    repeat = 0
 
     try:
-        while count < LIMIT:
-            password = make_password(count, PASSWORD_LENGTH)
-            count += 1
+        while count.value < LIMIT:
+            if stop_event.is_set():
+                raise KeyboardInterrupt()
+            
+            with count.get_lock():
+                count.value += 1
+                repeat = count.value - 1
+
+            password = make_password(repeat, PASSWORD_LENGTH)
+
+
+                
+            # test
             # if password == TEST_PASSWORD:
             #     break
 
             now = time.time()
             if now - last_report >= 1:
                 elasped = now - start_time
-                print(f'시작 시간: {started_at} | 진행 시간: {elasped:8.2f}s | 반복 횟수: {count:12d} | 비밀번호: {password:6s}')
+                print(f'{multiprocessing.current_process().name}, 시작 시간: {started_at} | 진행 시간: {elasped:8.2f}s | 반복 횟수: {repeat:12d} | 비밀번호: {password:6s}')
                 last_report = now
+
 
             try:
                 with pyzipper.AESZipFile(ZIP_PATH) as zf:
@@ -67,44 +74,82 @@ def unlock_zip():
 
                 with open(PASSWORD_PATH, 'w', encoding='utf-8') as f:
                     f.write(password)
+
+                stop_event.set()
+
                 break
             except (RuntimeError, pyzipper.BadZipFile, zlib.error):
                 continue
+            except KeyboardInterrupt:
+                raise KeyboardInterrupt()
     except KeyboardInterrupt:
-        print('\n시스템 종료\n')
-        exit(0)
+        print(f'{multiprocessing.current_process()}시스템 종료')
+        return
 
     total_elasped = time.time() - start_time
-    print(f'\n종료: 반복횟수: {count}, 진행 시간: {total_elasped:.2f}, 비밀번호: {password}')
+    print(f'\n{multiprocessing.current_process()} 종료: 반복횟수: {repeat}, 진행 시간: {total_elasped:.2f}, 비밀번호: {password}')
 
 
-def multi_process(stop_process_event: multiprocessing.context.BaseContext.Event):
+def multi_process(stop_process_event: EventClass):
     num_cores = multiprocessing.cpu_count()
 
-    processes = []
-    # for i in range(num_cores):
-        # p = multiprocessing.Process(target=worker, args=(i,))
-        # processes.append(p)
-        # p.start()
+    processes: list[Process] = []
+    count: Synchronized[int] = Value('i', 0)
+    for i in range(num_cores):
+        p = multiprocessing.Process(target=unlock_zip, args=(count, stop_process_event,))
+        processes.append(p)
+        p.start()
 
-    for p in processes:
-        p.join(timeout=TIME_OUT)
-    for p in processes:
-        if p.is_alive():
-            p.terminate()
-    for p in processes:
-        p.join()
+    return processes
 
 
 def main():
     try:
-        unlock_zip()
-    # except KeyboardInterrupt:
-    #     print('\n시스템 종료\n')
+        # unlock_zip()
+
+        stop_process_event = multiprocessing.Event()
+        processes = multi_process(stop_process_event)
+
+        while True:
+            time.sleep(0.2)
+
+    except KeyboardInterrupt:
+        print('\n시스템 종료\n')
     except Exception as e:
         print(f'[ERROR]: {e}')
         print(f'{e.__class__}')
+    finally:
+        stop_process_event.set()
+        for p in processes:
+            p.join(timeout=TIME_OUT)
+        for p in processes:
+            if p.is_alive():
+                p.terminate()
+        for p in processes:
+            p.join()
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        ctx = multiprocessing.get_context('spawn')
+
+        count: Synchronized[int] = ctx.Value('i', 0)
+        stop_event: EventClass = ctx.Event()
+
+        procs: list[Process] = [
+            ctx.Process(target=unlock_zip, args=(count, stop_event))
+            for _ in range(ctx.cpu_count())
+        ]
+
+        for p in procs: p.start()
+
+        while True:
+            if stop_event.wait(0.2):
+                break
+
+    except KeyboardInterrupt:
+        print('\n메인 프로세스 종료\n')
+    finally:
+        stop_event.set()
+        for p in procs: p.join()
+
