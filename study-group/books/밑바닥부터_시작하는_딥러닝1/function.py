4dd1bc1a06d55dd6e3489ef61c17e5ec5574deb0
LimJungWoo <presidentljw@naver.com>
2025-09-08 17:54:44 +0900

    feat(코디세이 스터디, 밑바닥부터 시작하는 딥러닝1): function.py - 계단 함수, 시그모이드 함수 코드 작성

---
+++ study-group/books/밑바닥부터_시작하는_딥러닝1/function.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+def step_function(x):
+    return np.array(x > 0, dtype=int)
+
+
+def sigmoid_function(x):
+    return 1 / (1 + np.exp(-x))
+
+
+if __name__ == '__main__':
+    x = np.arange(-5.0, 5.0, 0.1)
+    step_y = step_function(x)
+    sigmoid_y = sigmoid_function(x)
+    plt.plot(x, step_y)
+    plt.plot(x, sigmoid_y)
+    plt.ylim(-0.1, 1.1)
+    plt.show()
