f4504d2279020329f122abf54165bb0dd2e49fe2
LimJungWoo <presidentljw@naver.com>
2025-09-13 14:54:07 +0900

    feat(코디세이 스터디, 밑바닥부터 시작하는 딥러닝1): function.py - 소프트맥스 함수 코드 작성

--- study-group/books/밑바닥부터_시작하는_딥러닝1/function.py
+++ study-group/books/밑바닥부터_시작하는_딥러닝1/function.py
@@ -16,13 +16,24 @@ def relu(x):
 def identity_function(x):
     return x
 
+
+def softmax_function(x):
+    c = np.max(x)
+    exp_x = np.exp(x - c)   # 오버플로 대책
+    sum_exp_x = np.sum(exp_x)
+    y = exp_x / sum_exp_x
+    
+    return y
+
 if __name__ == '__main__':
     x = np.arange(-5.0, 5.0, 0.1)
     step_y = step_function(x)
     sigmoid_y = sigmoid_function(x)
     relu_y = relu(x)
+    softmax_y = softmax_function(x)
     plt.plot(x, step_y)
     plt.plot(x, sigmoid_y)
     plt.plot(x, relu_y)
+    plt.plot(x, softmax_y)
     plt.ylim(-0.1, 1.1)
     plt.show()
