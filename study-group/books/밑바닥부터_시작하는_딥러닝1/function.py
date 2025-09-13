b9bd73890b160b64f0cd363cc0468542f5bef25f
LimJungWoo <presidentljw@naver.com>
2025-09-13 13:53:54 +0900

    feat(코디세이 스터디, 밑바닥부터 시작하는 딥러닝1): function.py - ReLU 함수 코드 작성

--- study-group/books/밑바닥부터_시작하는_딥러닝1/function.py
+++ study-group/books/밑바닥부터_시작하는_딥러닝1/function.py
@@ -9,11 +9,17 @@ def sigmoid_function(x):
     return 1 / (1 + np.exp(-x))
 
 
+def relu(x):
+    return np.maximum(0, x)
+
+
 if __name__ == '__main__':
     x = np.arange(-5.0, 5.0, 0.1)
     step_y = step_function(x)
     sigmoid_y = sigmoid_function(x)
+    relu_y = relu(x)
     plt.plot(x, step_y)
     plt.plot(x, sigmoid_y)
+    plt.plot(x, relu_y)
     plt.ylim(-0.1, 1.1)
     plt.show()
